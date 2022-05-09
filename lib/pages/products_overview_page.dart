266c14ab788b601808c2ec0e0fbf8d7d183d2ad1
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-05-09 16:22:35 -0300

    Provider Implementation

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,31 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import '../models/product.dart';
-import '../models/product_list.dart';
+
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Shopping App'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        itemBuilder: (context, index) =>
-            ProductItem(product: loadedProducts[index]),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-      ),
+      body: ProductGrid(),
     );
   }
 }
