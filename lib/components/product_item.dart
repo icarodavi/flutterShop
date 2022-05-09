a88fb29027435cec03052b987e9846e263002c2a
Icaro Davi Tavares Monteiro <icarodavi@gmail.com>
2022-05-09 16:36:01 -0300

    Refactoring to toggle favorite

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
-import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
   const ProductItem({
     Key? key,
-    required this.product,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -33,9 +32,13 @@ class ProductItem extends StatelessWidget {
         footer: GridTileBar(
           backgroundColor: Colors.black54,
           leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite_border_outlined),
-            color: Theme.of(context).colorScheme.secondary,
+            onPressed: product.toggleFavorite,
+            icon: product.isFavorite
+                ? Icon(
+                    Icons.favorite,
+                    color: Theme.of(context).colorScheme.error,
+                  )
+                : Icon(Icons.favorite_border_outlined),
           ),
           title: Text(
             product.title,
@@ -44,7 +47,7 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: () {},
             icon: const Icon(Icons.shopping_cart),
-            color: Theme.of(context).colorScheme.secondary,
+            // color: Theme.of(context).colorScheme.secondary,
           ),
         ),
       ),
